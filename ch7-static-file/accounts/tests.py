a7365434fac8168c1a8fc823765918aa906daa61
Anastácio Paulino <anastaciopaulino008@gmail.com>
2023-12-04 21:23:45 +0100

    Static files and Bootstrap5

---
+++ ch7-static-file/accounts/tests.py
@@ -0,0 +1,50 @@
+from django.urls import reverse, resolve
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from .forms import CustomUserCreationForm
+from .views import SignUpPageView
+
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='anastacio', email='anastacio@gmail.com', password='testpass2023'
+        )
+        self.assertEqual(user.username, 'anastacio')
+        self.assertEqual(user.email, 'anastacio@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+    
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='superadmin', email='superadmin@gmail.com', password='testpass2023'
+        )
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'superadmin@gmail.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+
+class SignUpPageTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+    
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'registration/signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
+    
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+    
+    def test_signup_view(self):
+        view = resolve('/accounts/signup/')
+        self.assertEqual(view.func.__name__, SignUpPageView.as_view().__name__)
